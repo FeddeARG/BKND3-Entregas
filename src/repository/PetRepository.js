c7f05e5f3614754bf0008bcbd634ba6cb0ef0f28
FeddeARG <federicoanaranjo@gmail.com>
2024-11-08 01:09:04 -0300

    PreEntrega1-BKND3-Naranjo

---
+++ src/repository/PetRepository.js
@@ -0,0 +1,9 @@
+// src/repository/PetRepository.js
+
+import GenericRepository from "./GenericRepository.js";
+
+export default class PetRepository extends GenericRepository {
+    constructor(dao) {
+        super(dao);
+    }
+}
